f82ba4e39f18321a8323a874ee18ea7e1f3ac668
denningkevin <denningkev@gmail.com>
2019-11-04 10:25:23 +0300

    generate search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { GithubComponent } from './github/github.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     GithubComponent,
     HomeComponent,
     NavbarComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
