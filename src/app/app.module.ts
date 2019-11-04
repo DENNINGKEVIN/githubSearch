414f57f61d4de40d5236780ba6b38573874382d2
denningkevin <denningkev@gmail.com>
2019-11-04 10:43:33 +0300

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http';
 import { SearchFormComponent } from './search-form/search-form.component';
-import { HighlightDirective } from './highlight.directive';
+import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { RepositoryDetailsComponent } from './repository-details/repository-details.component';
 import { TimeCountPipe } from './time-count.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
-    HomeComponent,
-    NavbarComponent,
-    NotFoundComponent,
     SearchFormComponent,
-    HighlightDirective,
-    TimeCountPipe
+    NotFoundComponent,
+    NavbarComponent,
+    HomeComponent,
+    UserDetailsComponent,
+    RepositoryDetailsComponent,
+    TimeCountPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
